f581f73f5f34593f772aa3117d34adcc00fda2dc
Alex Garcia <lagg_85@hotmail.com>
2018-02-12 19:36:05 +0100

    Hecho el menu del blog

--- index.php
+++ index.php
@@ -1,56 +1,61 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package AlexGarcia
- */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
 
-		<?php
-		if ( have_posts() ) :
+			<div class="trabajo-principal">
+				<?php
+				    $trabajos = array( 'posts_per_page' => 1,
+				    				   'post_type' => 'post',
+				    				   'offset' => 0);
+				    $loop = new WP_Query( $trabajos );
+				    if ($loop->have_posts()) : while ($loop->have_posts()) : $loop->the_post(); ?>
+				    
+				       
+				       <a href="<?php the_permalink(); ?>"><div class="post-thumbnail1">
+				       		<h2><?php the_title(); ?></h2>
+				 			<?php the_post_thumbnail();?>
+				 			<span class="dashicons dashicons-plus-alt seguir-leyendo"></span>
+				 		</div></a> 
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+				         <?php endwhile;
+				          endif; 
+					wp_reset_query(); 
+				?>
+			</div>
 
-			<?php
-			endif;
+			<div class="resto-trabajos">
+				<?php
+				    $trabajos = array( 'posts_per_page' => 6,
+				    				   'post_type' => 'post',
+				    				   'offset' => 1 );
+				    $loop = new WP_Query( $trabajos );
+				     if ($loop->have_posts()) : while ($loop->have_posts()) : $loop->the_post(); ?>
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+				    
+				 	<div class="trabajo">
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				       <div class="post-thumbnail">
+				 			<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+				 		</div>
 
-			endwhile;
+				 		<span class="dashicons dashicons-plus-alt seguir-leyendo"></span>
 
-			the_posts_navigation();
+				 		<h2><?php the_title(); ?></h2>
+				    	<?php the_excerpt(); ?>	
+				    </div>
+				    	  
 
-		else :
+				         <?php endwhile; 
+				     endif; 
+					wp_reset_query(); 
+				?>
+			</div>	
 
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
+		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
+
+<?php get_footer(); ?>
